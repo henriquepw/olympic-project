b0284e91a1fcc2f6d7f2779521f09ce6ac8681fd
Henrique Martins <enrque.ns@gmail.com>
2019-08-16 08:20:57 -0300

    chore: environment variables

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <Container>
       <p>Contato</p>
-      <p>projeto.olimpico.cg@gmail.com</p>
+      <p>{process.env.REACT_APP_EMAIL}</p>
     </Container>
   );
 }
